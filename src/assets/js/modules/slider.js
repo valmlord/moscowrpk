ed711f2c63c5349ca14e8de1fa148b5b8e3bc642
Korshukov Stepan <korshukovstepan@gmail.com>
2021-06-20 10:12:22 +0300

    add footer html structure

--- src/assets/js/modules/slider.js
+++ src/assets/js/modules/slider.js
@@ -9,4 +9,16 @@ $(document).ready(function(){
         autoplaySpeed: 2000,
         arrows: false
     });
+
+    $('.mockup-reviews__slider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        arrows: true,
+        appendArrows: ('.mockup-reviews__slider-nav'),
+        prevArrow: '<button id="prev" type="button" class="btn btn-prev"><span class="icon-prev"></span></button>',
+        nextArrow: '<button id="next" type="button" class="btn btn-next"><span class="icon-next"></span></button>',
+        fade: true
+    });
   });
